fd7ed35ebd93cb1c7660556463bd1757e3486253
jjakkc <7483411+jjakkc@users.noreply.github.com>
2020-09-19 23:30:54 -0400

    Initial commit

---
+++ Assets/move.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class move : MonoBehaviour
+{
+    [SerializeField] float screenWidthInUnits = 16f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float mousePosInUnits = Input.mousePosition.x / Screen.width * screenWidthInUnits;
+        Vector3 playerPos = new Vector3(mousePosInUnits, transform.position.y, transform.position.z);
+
+        transform.position = playerPos;
+    }
+}
